a5a1ffbb04a1da7fca25e32dfaad15b8f82b8542
ehunten <e.hunten@gmail.com>
2016-02-19 12:34:33 -0700

    Working initializationgit push Print string not working.

---
+++ build/default/debug/lcd.o.d
@@ -0,0 +1,14 @@
+build/default/debug/lcd.o:  \
+lcd.c  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/xc.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/proc/p32mx470f512l.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/proc/ppic32mx.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/cp0defs.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/sys/appio.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/string.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/stddef.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/stdio.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/stdarg.h  \
+ ../../../../../../Program\ Files\ (x86)/Microchip/xc32/v1.40/pic32-libs/include/sys/l1cache.h  \
+ lcd.h  \
+timer.h 
